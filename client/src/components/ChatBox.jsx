046a6b726ada03ff4a4ebf80c19eb8ff24a640e4
Pavan-2929 <kapadiyapavan3218@gmail.com>
2024-02-28 00:48:32 +0530

    updated ChatBox data with backend data

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,40 +1,59 @@
-// ChatBox.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RegisterImage from "../assets/RegisterImage.png";
+import axios from "axios";
+import {format} from 'timeago.js'
 
-const ChatBox = ({ own }) => {
+const ChatBox = ({message, own }) => {
+
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/user/get/${message.senderId}`
+        );
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUser();
+  }, [])
+
   return (
     <>
       {own ? (
         <>
           <div className="flex mt-4 items-center justify-end">
             <div className="bg-[#444] rounded-lg p-3 max-w-[600px]">
-              <p className="text-sm">Hello guys, chai pi</p>
+              <p className="text-sm">{message.text}</p>
             </div>
             <img
-              src={RegisterImage}
+              src={user?.profilePicture}
               alt=""
               className="w-10 h-10 rounded-full ml-4"
             />
           </div>
           <div className="text-xs text-gray-500 ml-2 flex justify-end">
-            1 Hour ago
+            {format(message.createdAt)}
           </div>
         </>
       ) : (
         <>
           <div className="flex mt-4 items-center justify-start">
             <img
-              src={RegisterImage}
+              src={user?.profilePicture}
               alt=""
               className="w-10 h-10 rounded-full mr-4"
             />
             <div className="bg-[#444] rounded-lg p-3 max-w-[600px]">
-              <p className="text-sm">Hello guys, chai pi</p>
+              <p className="text-sm">{message.text}</p>
             </div>
           </div>
           <div className="text-xs text-gray-500 ml-2 flex justify-start">
-            1 Hour ago
+            {format(message.createdAt)}
           </div>
         </>
       )}
--- client/src/pages/Chats.jsx
+++ client/src/pages/Chats.jsx
@@ -1,4 +1,3 @@
-// Chats.js
 import React, { useEffect, useState } from "react";
 import ChatBox from "../components/ChatBox";
 import Conversation from "../components/Conversation";
@@ -8,6 +7,8 @@ import axios from "axios";
 const Chats = () => {
   const currentUser = useSelector((state) => state.currentUser);
   const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const fetchConversations = async () => {
@@ -24,22 +25,54 @@ const Chats = () => {
 
     fetchConversations();
   }, [currentUser]);
-  
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/message/get/${currentChat?._id}`
+        );
+        setMessages(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchMessages();
+  }, [currentChat]);
+
+  console.log(messages);
+
   return (
     <div className="w-[80vw] h-auto  mx-auto flex ">
       <div className="flex flex-col w-[20vw] bg-[#444]">
         <h1 className="text-center flex justify-center mt-5 text-3xl h-[2rem]">
           Users
         </h1>
-        {conversations && conversations.map((c) => (
-          <Conversation key={c._id} conversation={c} currentUser={currentUser}/>
-        ))}
+        {conversations &&
+          conversations.map((c) => (
+            <div onClick={() => setCurrentChat(c)} key={c._id}>
+              <Conversation conversation={c} currentUser={currentUser} />
+            </div>
+          ))}
       </div>
-      <div className="flex flex-col gap-2 p-4 bg-[#282828] w-[100%]">
-        <ChatBox />
-        <ChatBox own={true} />
-        <ChatBox />
-      </div>
+      {currentChat ? (
+        <>
+          <div className="flex flex-col gap-2 p-4 bg-[#282828] w-[100%]">
+            {
+              messages && messages.map((m) => (
+                <div key={m._id}>
+                  <ChatBox message={m} own={m.senderId === currentUser._id}/>
+                </div>
+              ))
+            }
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-col gap-2 p-4 bg-[#282828] w-[100%] text-center text-4xl ">
+          No chat found
+        </div>
+      )}
     </div>
   );
 };
