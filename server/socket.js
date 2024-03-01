c808c9d51c5cfc19c70a8ba03c821c1d73001513
Pavan-2929 <kapadiyapavan3218@gmail.com>
2024-03-01 11:31:26 +0530

    fixed issue related to the socket

--- server/socket.js
+++ server/socket.js
@@ -12,35 +12,39 @@ const addUsers = (userId, socketId) => {
 };
 
 const removeUser = (socketId) => {
-  users = users.filter(user => user.socketId !== socketId)
-}
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId) 
-}
+  return users.find((user) => user.userId === userId);
+};
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 
   socket.on("addUser", (userId) => {
-    addUsers(userId, socket.id)
-    io.emit("getUsers", users)
-  })
+    addUsers(userId, socket.id);
+    io.emit("getUsers", users);
+  });
 
-  socket.on("sendMessage", ({senderId, receiverId, text}) => {
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
 
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
-    })
-  })
-  
+    if (user) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    } else {
+      console.log("User not found");
+    }
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
-    removeUser(socket.id)
-    io.emit("getUsers", users)
-  })
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
 
 export default io;
